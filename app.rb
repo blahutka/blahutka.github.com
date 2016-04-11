fb8703612c6ff1baf4d7e7d6e35d5bad257744a3
Peter Blahutka <blahutka@centrum.cz>
2016-04-11 17:34:11 +0200

    update portfolio

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require 'sinatra/cache'
 require './site/db/models'
 require "sinatra/advanced_routes"
-require './site/views/cells/posts_cell'
+# require './site/views/cells/posts_cell'
 require 'sinatra/partial'
 require 'ostruct'
 require 'active_support'
@@ -23,7 +23,7 @@ class Portfolio < Sinatra::Base
 
   configure :development do
     register Sinatra::Reloader
-    also_reload File.join(Portfolio.root, 'site/cells/posts/*.*')
+    # also_reload File.join(Portfolio.root, 'site/cells/posts/*.*')
     register Sinatra::AdvancedRoutes
   end
 
@@ -43,8 +43,28 @@ class Portfolio < Sinatra::Base
 
 
     set :portfolios do
-      [
-          OpenStruct.new(:id => 'najdijob', :name => 'Najdijob.cz', :description => "Job search engine.
+      [OpenStruct.new(id: 'trackwave', name: 'Trackwave.com/Cor.us, USA', description: 'Cor.us is Survey application, that collects data from customers in a store.
+                      I worked on the admin part of the application. Ruby, GUI HTML/CSS, Javascript',
+                      :gems => [gem(:name => 'rails', full_name: 'Rails', version: '3.2', featured: true),
+                                gem(name: 'twitter-bootstrap-rails', full_name: 'Twitter Bootstrap', version: '3.2', fatured: true),
+                                gem(name: 'coffee-rails', full_name: 'Coffee Script'),
+                                gem(name: 'sass-rails', full_name: 'Sass CSS') ,
+                                gem(name: 'ruby', full_name: 'Ruby 1.9', version: '1.9'),
+                                gem(:full_name => 'jQuery', name: 'jquery-rails', :url => 'http://jquery.com')
+
+                      ]),
+       OpenStruct.new(id: 'brian', name: 'Hyper Local Media Inc, USA',
+                      description: 'CMS for local Newspapers. It was old web app which I converted to Twitter Bootstrap.
+                      Simultaneously I was adding new functions (Ruby, Javascript)', gems: [
+               gem(:name => 'rails', :full_name => 'Ruby on Rails', :version => '3.2', :featured => true),
+               gem(name: 'twitter-bootstrap-rails', full_name: 'Twitter Bootstrap', version: '2.2', fatured: true),
+               gem(name: 'sass-rails', full_name: 'Sass CSS'),
+               gem(name: 'ruby', full_name: 'Ruby 1.9', version: '1.9'),
+               gem(name: 'rails-stripe', full_name: 'Stripe payment'),
+               gem(:name => 'jquery-rails', :full_name => 'jQuery', :url => 'http://jquery.com')
+           ]),
+
+       OpenStruct.new(:id => 'najdijob', :name => 'Najdijob.cz', :description => "Job search engine.
               I build this application for myself. Server setup (capistrano), data grabbing
               from web. Data synchronization with Amazon S3 and web server.",
                          :gems => [
@@ -65,19 +85,33 @@ class Portfolio < Sinatra::Base
                              gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
                              gem(:name => 'Css', :url => 'http://www.w3.org/Style/Overview.html')
                          ]),
-          OpenStruct.new(:id => 'adw', :name => 'Adwave.com', :description => "AdWave is a free cross-promotional
+       OpenStruct.new(:id => 'adw', :name => 'Adwave.com, USA/CZ', :description => "AdWave is a free cross-promotional
                          and advertising network enabling developers,
                         advertisers and publishers to grow their social apps.",
-                         :gems => [
+                      :gems => [
                              gem(:name => 'sass', :featured => true),
                              gem(:name => 'jQuery', :url => 'http://jquery.com', :featured => true),
                              gem(:name => 'html', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc', :featured => true)
                          ]),
 
-          OpenStruct.new(:id => 'dentservis', :name => 'Dentservis.cz', :description => "Contract management
+       OpenStruct.new(id: 'cleaning_service', name: 'CleaningService (prototyping)',
+                      description: 'Web app, Managing time and People. I created partially functional prototype (rails application without connection to database)', gems: [
+           gem(:name => 'rails', :full_name => 'Ruby on Rails', :version => '3.2', :featured => true),
+           gem(:name => 'sass', :featured => true),
+           gem(name: 'twitter-bootstrap-rails', full_name: 'Twitter Bootstrap', version: '2.2', fatured: true),
+       ]),
+
+       OpenStruct.new(id: 'adeon', name: 'Teaching app (prototyping)',
+                      description: 'Web app for learning and teaching. I created partially functional prototype (rails application without connection to database)', gems: [
+           gem(:name => 'rails', :full_name => 'Ruby on Rails', :version => '3.2', :featured => true),
+           gem(:name => 'sass', :featured => true),
+           gem(name: 'twitter-bootstrap-rails', full_name: 'Twitter Bootstrap', version: '2.2', fatured: true),
+       ]),
+
+       OpenStruct.new(:id => 'dentservis', :name => 'Dentservis.cz, CZ', :description => "Contract management
                       for Dentist Laboratories. This is first stage prototyping (TwitterBootstrap) and design UI.
                       Some RubyOnRails setup DB and structure",
-                         :gems => [
+                      :gems => [
                              gem(:name => 'rails', :version => '3.2'),
                              gem(:name => 'pg', :full_name => 'PostgreSql'),
                              gem(:name => 'sassy_noise'),
@@ -104,63 +138,34 @@ class Portfolio < Sinatra::Base
                              gem(:name => 'faker')
                          ]),
 
-          OpenStruct.new(:id => 'ftuner', :name => 'Facetuner.com', :description => 'Social application. I worked
+       OpenStruct.new(:id => 'ftuner', :name => 'Facetuner.com, USA/CZ', :description => 'Social application. I worked
                      as HTML/CSS webmaster',
-                         :gems => [
+                      :gems => [
                              gem(:name => 'jQuery', :url => 'http://jquery.com'),
                              gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
                              gem(:name => 'sass')
                          ]),
 
-          OpenStruct.new(:id => 'pservis', :name => 'PohodovyServis.cz', :description => "Customer service
-                     for electronics, PC etc. One place to deal with you service. I worked on whole application",
-                         :gems => [
-                             gem(:name => 'rails', :version => '3.2.3'),
-                             gem(:name => 'mysql2'),
-                             gem(:name => 'sass-rails'),
-                             gem(:name => 'less-rails-bootstrap'),
-                             gem(:name => 'bootstrap-datepicker-rails'),
-                             gem(:name => 'simple_state_machine'),
-                             gem(:name => 'will_paginate'),
-                             gem(:name => 'inherited_resources'),
-                             gem(:name => 'simple_form'),
-                             gem(:name => 'erector'),
-                             gem(:name => 'mini_record'),
-                             gem(:name => 'meta_search'),
-                             gem(:name => 'friendly_id'),
-                             gem(:name => 'guard'),
-                             gem(:name => 'factory_girl'),
-                             gem(:name => 'rack-livereload'),
-                             gem(:name => 'capistrano'),
-                             gem(:name => 'jQuery', :url => 'http://jquery.com'),
-                             gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc')
-                         ]),
 
-          OpenStruct.new(:id => 'rgiant', :name => 'RebateGiant',
-                         :description => "RebateGiant offers consumers money-saving services including cash back rebates, discount coupons, special promotions and free shipping offers from top brand providers.",
-                         :gems => [
+       OpenStruct.new(:id => 'rgiant', :name => 'RebateGiant, USA/CZ',
+                      :description => "RebateGiant offers consumers money-saving services including cash back rebates, discount coupons, special promotions and free shipping offers from top brand providers.",
+                      :gems => [
                              gem(:name => 'jQuery', :url => 'http://jquery.com'),
                              gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
                              gem(:name => 'sass')
                          ]),
+       OpenStruct.new(:id => 'ovault', :name => 'OnlineVault.com, USA/CZ',
+                      :description => 'small yet powerful application that automatically backs up all your important data to secure servers but also makes it possible for you to access it on the go!',
+                      :gems => [
+                          gem(:name => 'jQueryMobile'),
+                          gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
+                          gem(:name => 'Css', :url => 'http://www.w3.org/Style/Overview.html')
+                      ]),
 
-          OpenStruct.new(:id => 'strechy-ondrik', :name => 'Strechy-Ondrik.cz',
-                         :description => 'Small presentation for an roofer guy. Padrino framework',
-                         :gems => [
-                             gem(:name => 'sinatra'),
-                             gem(:name => 'padriono'),
-                             gem(:name => 'less'),
-                             gem(:name => 'sass'),
-                             gem(:name => 'will_paginate'),
-                             gem(:name => 'erubis'),
-                             gem(:name => 'guard'),
-                             gem(:name => 'html', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
-                             gem(:name => 'jQuery', :url => 'http://jquery.com')
-                         ]),
 
-      OpenStruct.new(:id => 'strelecdent', :name => 'StrelecDent.cz',
-                     :description => 'Small presentation site for Dentist laboratory. Ruby on Rails',
-                     :gems => [
+       OpenStruct.new(:id => 'strelecdent', :name => 'StrelecDent.cz, CZ',
+                      :description => 'Small presentation site for Dentist laboratory. Ruby on Rails',
+                      :gems => [
                          gem(:name => 'rails', :version => '3.0.0'),
                          gem(:name => 'mysql2'),
                          gem(:name => 'compass'),
@@ -174,13 +179,42 @@ class Portfolio < Sinatra::Base
                          gem(:name => 'paperclip')                      ,
                      ]),
 
-      OpenStruct.new(:id => 'ovault', :name => 'OnlineVault.com',
-                     :description => 'small yet powerful application that automatically backs up all your important data to secure servers but also makes it possible for you to access it on the go!',
-                     :gems => [
-                         gem(:name => 'jQueryMobile'),
-                         gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
-                         gem(:name => 'Css', :url => 'http://www.w3.org/Style/Overview.html')
-                     ])
+       OpenStruct.new(:id => 'strechy-ondrik', :name => 'Strechy-Ondrik.cz, CZ',
+                      :description => 'Small presentation for an roofer guy. Padrino framework',
+                      :gems => [
+                          gem(:name => 'sinatra'),
+                          gem(:name => 'padriono'),
+                          gem(:name => 'less'),
+                          gem(:name => 'sass'),
+                          gem(:name => 'will_paginate'),
+                          gem(:name => 'erubis'),
+                          gem(:name => 'guard'),
+                          gem(:name => 'html', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc'),
+                          gem(:name => 'jQuery', :url => 'http://jquery.com')
+                      ]),
+       OpenStruct.new(:id => 'pservis', :name => 'PohodovyServis.cz, CZ', :description => "Customer service
+                    for electronics, PC etc. One place to deal with you service. I worked on whole application",
+                      :gems => [
+                          gem(:name => 'rails', :version => '3.2.3'),
+                          gem(:name => 'mysql2'),
+                          gem(:name => 'sass-rails'),
+                          gem(:name => 'less-rails-bootstrap'),
+                          gem(:name => 'bootstrap-datepicker-rails'),
+                          gem(:name => 'simple_state_machine'),
+                          gem(:name => 'will_paginate'),
+                          gem(:name => 'inherited_resources'),
+                          gem(:name => 'simple_form'),
+                          gem(:name => 'erector'),
+                          gem(:name => 'mini_record'),
+                          gem(:name => 'meta_search'),
+                          gem(:name => 'friendly_id'),
+                          gem(:name => 'guard'),
+                          gem(:name => 'factory_girl'),
+                          gem(:name => 'rack-livereload'),
+                          gem(:name => 'capistrano'),
+                          gem(:name => 'jQuery', :url => 'http://jquery.com'),
+                          gem(:name => 'HTML', :url => 'http://www.w3.org/TR/html4/cover.html#minitoc')
+                      ])
 
       ]
     end
--- site/public/javascripts/application.js
+++ site/public/javascripts/application.js
@@ -44,10 +44,62 @@ $(document).ready(function () {
         ],
         "ovault":[
             ['site/public/images/ovault/home.png']
+        ],
+        "trackwave": [
+            ['site/public/images/trackwave/01.png', 'Search for multiple objects Users/Surveys'],
+            ['site/public/images/trackwave/14.png', 'Visually edit survey and add to folders'],
+            ['site/public/images/trackwave/02.png', 'Drag&Drop survey to folders'],
+            ['site/public/images/trackwave/03.png', 'Questions search dropdown'],
+            ['site/public/images/trackwave/04.png', 'Surfboards management'],
+            ['site/public/images/trackwave/05.png', 'Surveys management'],
+            ['site/public/images/trackwave/06.png', 'Dropdown - embed survey Widget'],
+            ['site/public/images/trackwave/07.png', 'Dropdown - links to share public survey'],
+            ['site/public/images/trackwave/08.png', 'Dropdown - Languages for sruvey'],
+            ['site/public/images/trackwave/09.png', 'Dropdown - Add new survey member'],
+            ['site/public/images/trackwave/16.png', 'Dropdown - Manage questions for survey'],
+            ['site/public/images/trackwave/19.png', 'Dropdown - Add survey to surfboards'],
+            ['site/public/images/trackwave/10.png', 'Dropdown - Share survey'],
+            ['site/public/images/trackwave/12.png', 'Edit survey data'],
+            ['site/public/images/trackwave/13.png', 'Edit survey data'],
+            ['site/public/images/trackwave/15.png', 'Active users'],
+            ['site/public/images/trackwave/21.png', 'Survey participant roles'],
+            ['site/public/images/trackwave/22.png', 'Manage backend permissions']
+        ],
+        "brian": [
+            ['site/public/images/brian/01.png', ''],
+            ['site/public/images/brian/02.png', ''],
+            ['site/public/images/brian/03.png', ''],
+            ['site/public/images/brian/04.png', ''],
+            ['site/public/images/brian/05.png', ''],
+            ['site/public/images/brian/06.png', ''],
+            ['site/public/images/brian/07.png', ''],
+            ['site/public/images/brian/08.png', ''],
+            ['site/public/images/brian/09.png', ''],
+            ['site/public/images/brian/10.png', ''],
+            ['site/public/images/brian/11.png', ''],
+            ['site/public/images/brian/12.png', ''],
+            ['site/public/images/brian/13.png', ''],
+            ['site/public/images/brian/14.png', ''],
+            ['site/public/images/brian/15.png', ''],
+            ['site/public/images/brian/16.png', ''],
+            ['site/public/images/brian/17.png', ''],
+            ['site/public/images/brian/18.png', ''],
+            ['site/public/images/brian/19.png', '']
+        ], "cleaning_service": [
+            ['site/public/images/cleaning_service/01.png', ''],
+            ['site/public/images/cleaning_service/02.png', ''],
+            ['site/public/images/cleaning_service/03.png', ''],
+            ['site/public/images/cleaning_service/04.png', ''],
+            ['site/public/images/cleaning_service/05.png', ''],
+        ], "adeon": [
+            ['site/public/images/adeon/00.png', ''],
+            ['site/public/images/adeon/01.png', ''],
+            ['site/public/images/adeon/02.png', ''],
+            ['site/public/images/adeon/03.png', '']
         ]
     };
 
-    $('.thumbnail a').click(function () {
+    $('a.show-gallery').click(function () {
         var id = $(this).attr('id');
         jQuery.slimbox(portfolioImages[id], 0);
         return false;
